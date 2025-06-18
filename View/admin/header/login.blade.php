bb6d6ba25dee8d9c1948cf9d2d5626950db412b4
Durga Kishore <chitturidurgakishore@gmail.com>
2025-06-18 22:02:14 +0530

    Create login.blade.php

---
+++ View/admin/header/login.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous">
+    </script>
+    <title>Admin Login</title>
+    <style>
+        .form-bt {
+            width: 500px;
+            height: 500px;
+            background-color: aquamarine;
+            margin: 30px;
+            padding: 15px;
+            border-radius: 20px;
+        }
+
+        #exampleInputEmail1 {
+            width: 150px;
+        }
+
+        #exampleInputPassword1 {
+            width: 150px;
+        }
+        .formdata{
+            margin: 60px;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <div class=form-bt>
+            <h1>Login Admin</h1>
+            <form class="formdata" method="POST" action="/LoginCheck">
+                @csrf
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </center>
+</body>
+
+</html>
